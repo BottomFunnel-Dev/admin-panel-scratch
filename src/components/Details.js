204abb1c5904ae0d00efaaae5aede99a8d6cf54b
hemantjayas <hemantjayas03@gmail.com>
2022-12-28 13:59:57 +0530

    changes

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import "./details.css";
 import { useNavigate } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Details = () => {
   const [logindata, setLoginData] = useState([]);
+  const [expand, setexpand] = useState(false);
+
+  const handledrop = () => {
+    expand ? setexpand(false) : setexpand(true);
+  };
 
   const history = useNavigate();
 
@@ -49,32 +57,32 @@ const Details = () => {
       {logindata.length === 0 ? (
         "errror"
       ) : (
-        <>
-          <h1>detials page</h1>
-          <h1>{logindata[0].name}</h1>
-          <Button onClick={userlogout}>LogOut</Button>
-
-          {logindata[0].date === todayDate ? (
-            <Modal show={show} onHide={handleClose}>
-              <Modal.Header closeButton>
-                <Modal.Title>{logindata[0].name} 😄</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                Wish you many many happy returns of the day ! 🍰
-              </Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                  Save Changes
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          ) : (
-            ""
+        <div>
+<div className="logout-button">
+  <button onClick={userlogout}>Logout</button>
+  </div>
+        <div className="sidenav">
+          <div className="sidenaveItem">Dashboard</div>
+          <div className="sidenaveItem innerside" onClick={handledrop}>
+            <p>Modules</p>
+           <div className="dropImage"><img src="Images/down.png" alt=""/></div>
+          </div>
+          {expand && (
+            <div className="modules">
+              <div className="sidenaveItem">Module 1</div>
+              <div className="sidenaveItem">Module 2</div>
+             
+            </div>
           )}
-        </>
+
+          <div className="sidenaveItem">
+            ERP Solutions drop <RiArrowDropDownLine />
+          </div>
+          <div className="sidenaveItem">API Sourcing and </div>
+          <div className="sidenaveItem">Dashboard</div>
+          <div className="sidenaveItem">Dashboard</div>
+        </div>
+          </div>
       )}
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,17 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Header = () => {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <img src="Images/" alt=""/>
-          <p style={{color:"white" , margin:0}}>Welcome to the Bottom Funnel Admin panel</p>
+          <p style={{ color: "white", margin: 0, padding:"10px 0" }}>
+            Welcome to the Bottom Funnel Admin panel
+          </p>
+          {/* <Button className="">Logout</Button> */}
         </Container>
       </Navbar>
     </>
